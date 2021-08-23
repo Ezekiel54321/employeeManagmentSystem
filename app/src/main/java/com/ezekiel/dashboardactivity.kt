91d3bfc668add6d9572fb9ea8dcddf31cb89b41e
Ezekiel12345 <you@example.com>
2021-08-23 18:01:17 +0100

    EMS dashboard code

---
+++ app/src/main/java/com/ezekiel/dashboardactivity.kt
@@ -0,0 +1,12 @@
+package com.ezekiel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ezekiel.employeemanagmentsystem.R
+
+class dashboardactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboardactivity)
+    }
+}
