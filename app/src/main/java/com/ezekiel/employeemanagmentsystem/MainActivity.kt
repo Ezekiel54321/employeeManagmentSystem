e451f318d7b0da62f10d9b7cd8663f239f012fd6
Ezekiel12345 <you@example.com>
2021-08-28 02:09:21 +0100

    i built the profile page but its not running on my device, pls try on yours

--- app/src/main/java/com/ezekiel/employeemanagmentsystem/MainActivity.kt
+++ app/src/main/java/com/ezekiel/employeemanagmentsystem/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ezekiel.employeemanagmentsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list : MutableList<String> = ArrayList()
+        for(i in 1..500)
+            list.add("item $i")
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter ( this, R.layout.support_simple_spinner_dropdown_item, list)
+        adapter
     }
 }
